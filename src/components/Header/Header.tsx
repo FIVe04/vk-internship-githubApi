218f51bf42fae6554bfddc3043886502f41621dd
FIVe04 <ivan.a.frolov@gmail.com>
2025-01-15 15:57:51 +0300

    added link to github in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,10 +5,10 @@ const Header = () => {
   return (
     <header className={styles.header}>
         <h1 className={styles.title}>Gihub Python Repositories</h1>
-        <div className={styles.logoOuter}>
+        <a className={styles.logoOuter} target='_blank' href='https://github.com/FIVe04/vk-internship-githubApi/'>
             <h2 className={styles.logoTitle}>@five04</h2>
             <img src={githibLogo} alt="Github Logo" className={styles.logo} />
-        </div>
+        </a>
     </header>
   )
 }
